3ced948a859f34a220ac7fc9aa4f53ce9fa224d5
MAYSA SILVA <maysa.silva4@fatec.sp.gov.br>
2023-03-10 19:53:35 -0300

    Add project files.

---
+++ ConsoleApp_Noite/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Noite
+{
+    public class Produto
+    {
+        private int Id;
+        private decimal Valor;
+        public string Descricao;
+
+        public Produto(int id,decimal valor, string descricao )
+        {
+            Id = id;
+            Descricao = descricao;
+            Valor = valor;
+        }
+    }
+}
---
+++ ConsoleApp_Noite/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_Noite;
+using System.Runtime.CompilerServices;
+
+Produto p1 = new Produto(1, 10.90M, "Coca-Cola");
+Console.WriteLine("");
