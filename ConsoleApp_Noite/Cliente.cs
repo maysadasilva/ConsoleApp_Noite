8ac243ed7a052c1b6000175904b168504cfb91ca
MAYSA SILVA <maysa.silva4@fatec.sp.gov.br>
2023-03-10 20:25:04 -0300

    Criação da class cliente e adicação dos métodos get e set

---
+++ ConsoleApp_Noite/Cliente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Noite
+{
+    public class Cliente
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public void SetNome(string nome)
+        {
+            Nome = nome;
+            if(nome !="")
+            {
+                Nome = nome;
+            }
+        }
+
+        public void SetTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+    }
+}
